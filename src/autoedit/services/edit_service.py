8f852b865170ea1ec8735a7b6756dbd97585e0d2
SvenPfiffner <sven.pfiffner@gmail.com>
2025-10-01 23:00:28 +0200

    Hook up qwen image

--- src/autoedit/services/edit_service.py
+++ src/autoedit/services/edit_service.py
@@ -1,10 +1,41 @@
+import os
 from typing import Callable, List, Optional
+import io
+
+
+from PIL import Image
+import torch
+
+from diffusers import QwenImageEditPipeline
 
 def edit_image(image_bytes: bytes, refined_prompt: str) -> Optional[bytes]:
         
-    if not image_bytes:
-        return None
+    image = Image.open(io.BytesIO(image_bytes))
     
-    # Real implementation would return the edited image. For now we simply
-    # echo the input bytes so that the UI can render the uploaded image.
-    return image_bytes
+    model_path = "ovedrive/qwen-image-edit-4bit"
+    pipeline = QwenImageEditPipeline.from_pretrained(model_path, torch_dtype=torch.bfloat16)
+    print("pipeline loaded") # not true but whatever. do not move to cuda
+
+    pipeline.set_progress_bar_config(disable=None)
+    pipeline.to("cuda") #if you have enough VRAM replace this line with `pipeline.to("cuda")` which is 20GB VRAM
+    prompt = "Make the man wear clown makeup and a clown nose. mantain the character face, eyes, skin details, lightning, pose, position and overall composition)"
+    inputs = {
+        "image": image,
+        "prompt": prompt,
+        "generator": torch.manual_seed(0),
+        "true_cfg_scale": 4.0,
+        "negative_prompt": "low quality, bad anatomy, extra fingers, missing fingers, extra limbs, missing limbs",
+        "num_inference_steps": 20, # even 10 steps should be enough in many cases
+    }
+
+    with torch.inference_mode():
+        output = pipeline(**inputs)
+
+    pipeline.to("cpu")
+    torch.cuda.empty_cache()
+
+    output_image = output.images[0]
+    output_buffer = io.BytesIO()
+    output_image.save(output_buffer, format="PNG")
+    output_buffer.seek(0)
+    return output_buffer.getvalue()
